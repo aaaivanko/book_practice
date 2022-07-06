f3e51edf91cba4aa0550f611781a4dcbc74fbc8d
Ivan <ivan.chvartatskyy@gmail.com>
2022-07-06 23:17:08 +0200

    added chapter five simple programs

---
+++ the_book_python_cource/python_book/chapter_five/if_operator_2.py
@@ -0,0 +1,10 @@
+
+number = 6
+
+if (number > 0) and (number == 1):
+    print(f'{number}st')
+elif number < 4 and number != 0:
+    print(f'{number}nd')
+elif number < 10 and number != 0:
+    print(f'{number}th')
+
